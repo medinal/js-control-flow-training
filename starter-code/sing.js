3be74cfc1e94c5901a69d51043163b5507401159
Logan Medina <logan.medina@gmail.com>
2017-01-25 09:34:21 -0800

    fixed

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -2,8 +2,9 @@ console.log("sing.js loaded");
 
 do {
   var numBottles = parseInt(prompt("How many bottles of beer are on the wall?"));
+  console.log(numBottles);
 }
-while (typeof numBottles != 'number');
+while (isNaN(numBottles) || numBottles<=0);
 
 while (numBottles > 0){
   if (numBottles > 2){
@@ -11,12 +12,18 @@ while (numBottles > 0){
     console.log(numBottles + " bottles of beer!");
     console.log("Take one down and pass it around,");
     while (numBottles > 2){
+      console.log(numBottles + " bottles of beer on the wall,");
+      console.log(numBottles + " bottles of beer!");
+      console.log("Take one down and pass it around,");
       numBottles --;
       console.log(numBottles + " bottles of beer on the wall!");
       }
   } else if (numBottles === 2){
-      numBottles --;
-      console.log(numBottles + " bottle of beer on the wall!")
+    console.log(numBottles + " bottles of beer on the wall,");
+    console.log(numBottles + " bottles of beer!");
+    console.log("Take one down and pass it around,");
+    numBottles --;
+    console.log(numBottles + " bottle of beer on the wall!")
   } else if (numBottles === 1){
       console.log(numBottles + " bottle of beer on the wall,");
       console.log(numBottles + " bottle of beer!");
